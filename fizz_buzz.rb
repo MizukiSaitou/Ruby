1ff59bedb19f1b96f869379c4d957fe7679ec664
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-08-08 22:22:14 +0900

    some patterns

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -25,3 +25,87 @@ puts fizz_buzz(12)
 puts fizz_buzz(13)
 puts fizz_buzz(14)
 puts fizz_buzz(15)
+
+# each文
+(1..100).each do |n|
+  def fizz_buzz(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
+  puts fizz_buzz(n)
+end
+
+for文
+for n in 1..100
+  def fizz_buzz(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
+  puts fizz_buzz(n)
+end
+
+
+upto文
+1.upto(100) do |n|
+  def fizz_buzz(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
+  puts fizz_buzz(n)
+end
+
+
+ # times文
+1..101.times do |n|
+  def fizz_buzz(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
+  puts fizz_buzz(n)
+end
+
+# # while文
+n = 0
+while n < 100
+  def fizz_buzz(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
+  i = n += 1
+  puts fizz_buzz(i)
+end
